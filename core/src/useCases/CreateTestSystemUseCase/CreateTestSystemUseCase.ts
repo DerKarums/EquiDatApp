66b35228d899f76bda6d077d5b0baac292608e91
Lars Kecker <lars.kecker2009@gmail.com>
2021-09-30 14:28:38 +0200

    EQ-68 implemented CreateTestSystemUseCase

---
+++ core/src/useCases/CreateTestSystemUseCase/CreateTestSystemUseCase.ts
@@ -0,0 +1,21 @@
+import TestSystem from "../../entities/TestSystem";
+import { CreateTestSystemCallbacks } from "./CreateTestSystemCallbacks";
+import { CreateTestSystemRepository } from "./CreateTestSystemRepository";
+import TestSystemModel from "./TestSystemModel";
+
+
+export default class CreateTestSystemUseCase {
+
+    constructor(
+        private readonly repository: CreateTestSystemRepository,
+    ) {
+
+    }
+
+    public createTestSystem(testSystemModel: TestSystemModel, callbacks: CreateTestSystemCallbacks) {
+        const schema = this.repository.getSchema()
+        const testSystem = new TestSystem(schema, testSystemModel.systemPropertyValues);
+        this.repository.createTestSystem(testSystem);
+        callbacks.onComplete();
+    }
+}
