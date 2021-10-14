bdfbb7232d821e6bd81e00aab75a3f32b779180d
Nico Betger <nicobetger1@gmail.com>
2021-10-14 10:35:11 +0200

    EQ-73 Added core as frontend dependency

---
+++ core/src/entities/index.ts
@@ -0,0 +1,10 @@
+import Component from "./Component";
+import ComponentType from "./ComponentType";
+import ManifacturingUnit from "./ManufacturingUnit";
+import SubSystem from "./SubSystem";
+import SystemProperty from "./SystemProperty";
+import SystemPropertyType from "./SystemPropertyType";
+import TestSystem from "./TestSystem";
+
+
+export { Component, ComponentType, ManifacturingUnit, SubSystem, SystemProperty, SystemPropertyType, TestSystem }
