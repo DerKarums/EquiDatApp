3bbdbc4dd7a9a36b56350165b5f1025f1e331dd0
Lars Kecker <lars.kecker2009@gmail.com>
2022-02-10 16:39:14 +0100

    EQ-98 extract SubSystemPopupMenu to Shared component

--- frontend/src/components/detail/InnerSubsystemTable.tsx
+++ frontend/src/components/detail/InnerSubsystemTable.tsx
@@ -1,12 +1,7 @@
 import MoreVertIcon from "@material-ui/icons/MoreVert";
 import { Typography } from "@mui/material";
-import ClickAwayListener from "@mui/material/ClickAwayListener";
-import Grow from "@mui/material/Grow";
 import IconButton from "@mui/material/IconButton";
-import MenuItem from "@mui/material/MenuItem";
-import MenuList from "@mui/material/MenuList";
 import Paper from "@mui/material/Paper";
-import Popper from "@mui/material/Popper";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
@@ -14,7 +9,8 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import { SubSystem, SystemProperty } from "core";
-import React, { useRef, useState } from "react";
+import React, { useState } from "react";
+import SubSystemPopupMenu from "../shared/abstract/SubSystemTable/SubSystemPopupMenu";
 
 interface InnerSubSystemTableProps<SubSystemType extends SubSystem> {
   subSystems: SubSystemType[];
@@ -31,47 +27,14 @@ function InnerSubSystemTable<SubSystemType extends SubSystem>({
     useState<SubSystemType | null>(null);
   const [anchorRef, setAnchorRef] = useState<HTMLButtonElement | null>(null);
 
-  const handleShowDetails = (event: Event | React.SyntheticEvent) => {
-    handleClose(event);
-    if (selectedSubsystem === null) {
-      return;
-    }
-    selectSubSystem(selectedSubsystem.id);
+  const handleShowDetails = (selectedSubSystem: SubSystemType) => {
+    selectSubSystem(selectedSubSystem.id);
   };
 
-  const handleClose = (event: Event | React.SyntheticEvent) => {
-    if (
-      anchorRef &&
-      anchorRef.contains(event.target as HTMLElement)
-    ) {
-      return;
-    }
-    setSelectedSubsystem(null);
-  };
-
-  function handleListKeyDown(event: React.KeyboardEvent) {
-    if (event.key === "Tab") {
-      event.preventDefault();
-      setSelectedSubsystem(null);
-    } else if (event.key === "Escape") {
-      setSelectedSubsystem(null);
-    }
-  }
-
-  function clickMenu(subsystem: SubSystemType, event: React.MouseEvent<HTMLElement>) {
+  function openMenu(subsystem: SubSystemType, event: React.MouseEvent<HTMLElement>) {
     setAnchorRef(event.currentTarget as HTMLButtonElement);
     setSelectedSubsystem(subsystem);
   }
-
-  // return focus to the button when we transitioned from !open -> open
-  const prevOpen = React.useRef(selectedSubsystem !== null);
-  React.useEffect(() => {
-    if (prevOpen.current === true && selectedSubsystem === null) {
-      anchorRef?.focus();
-    }
-
-    prevOpen.current = selectedSubsystem !== null;
-  }, [selectedSubsystem]);
 
   const tableHeaderCellStyle = {
     fontWeight: "bold",
@@ -111,7 +74,7 @@ function InnerSubSystemTable<SubSystemType extends SubSystem>({
                     }
                     aria-expanded={selectedSubsystem ? "true" : undefined}
                     aria-haspopup="true"
-                    onClick={(e: React.MouseEvent<HTMLElement>) => clickMenu(subSystem, e)}
+                    onClick={(e: React.MouseEvent<HTMLElement>) => openMenu(subSystem, e)}
                   >
                     <MoreVertIcon />
                   </IconButton>
@@ -121,39 +84,16 @@ function InnerSubSystemTable<SubSystemType extends SubSystem>({
           </TableBody>
         </Table>
       </TableContainer>
-      <Popper
-        open={selectedSubsystem !== null}
+      <SubSystemPopupMenu<SubSystemType>
+        selectedSubSystem={selectedSubsystem}
         anchorEl={anchorRef}
-        role={undefined}
-        placement="bottom-start"
-        transition
-        disablePortal
-      >
-        {({ TransitionProps, placement }) => (
-          <Grow
-            {...TransitionProps}
-            style={{
-              transformOrigin:
-                placement === "bottom-start" ? "left top" : "left bottom",
-            }}
-          >
-            <Paper>
-              <ClickAwayListener onClickAway={handleClose}>
-                <MenuList
-                  autoFocusItem={selectedSubsystem !== null}
-                  id="composition-menu"
-                  aria-labelledby="composition-button"
-                  onKeyDown={handleListKeyDown}
-                >
-                  <MenuItem onClick={handleClose}>Duplizieren</MenuItem>
-                  <MenuItem onClick={handleClose}>Löschen</MenuItem>
-                  <MenuItem onClick={handleShowDetails}>Ansicht</MenuItem>
-                </MenuList>
-              </ClickAwayListener>
-            </Paper>
-          </Grow>
-        )}
-      </Popper>
+        menuEntries={[
+          {label: "Duplizieren", onClick: (_: SubSystemType) => {}},
+          {label: "Löschen", onClick: (_: SubSystemType) => {}},
+          {label: "Ansicht", onClick: handleShowDetails},
+        ]}
+        setSelectedSubSystem={setSelectedSubsystem}
+      />
     </>
   );
 }
